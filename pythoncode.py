ea6516a6c87303599c896dba44456b5bc5958f7b
hari krishna <mhari40@gmail.com>
2025-10-31 09:53:58 +0530

    adding list of operations

---
+++ pythoncode.py
@@ -0,0 +1,13 @@
+#python code for operations list 
+
+def calculator():
+    print("="*40)
+    print("🧠 SIMPLE CALCULATOR 🧠")
+    print("="*40)
+    print("Select operation:")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
+    print("5. Exit")
+    print("="*40)
